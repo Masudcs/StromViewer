ddb9425511ba658db92287259c94bdb8b541935c
Md. Masud Rana <masud.pust16@gmail.com>
2024-07-07 17:33:24 +0600

    Store asset image name in a array

---
+++ StromViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StromViewer
+//
+//  Created by Md. Masud Rana on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+}
